22f53b82ca96ce9ee6cedb83aa55cf4a17477ac1
Genna1 <Genna1@Jenna>
2017-10-08 09:03:41 -0500

    First Github commit.

---
+++ src/currencyconversion/StartUp.java
@@ -0,0 +1,17 @@
+
+package currencyconversion;
+
+/**
+ *
+ * @author Jenna
+ */
+public class StartUp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
